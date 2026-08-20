9969558a18b27f10d85870cbfc5d38ead11d5306
Frhstaaa <tiyasfrahesta888@gmail.com>
2026-08-20 13:48:32 +0700

    fix: Import User model in LeaveRequestController

--- app/Http/Controllers/LeaveRequestController.php
+++ app/Http/Controllers/LeaveRequestController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\LeaveCategory;
 use App\Models\LeaveQuota;
 use App\Models\LeaveRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
