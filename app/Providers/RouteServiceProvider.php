9aca542967c9373de1e5ef7dcef851aabff6357b
Frhstaaa <tiyasfrahesta888@gmail.com>
2026-08-23 18:01:27 +0700

    feat: register automatic leaves-application subfolder route group in RouteServiceProvider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/dashboard';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
@@ -31,7 +31,16 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
 
+            Route::middleware('api')
+                ->prefix('leaves-application/api')
+                ->group(base_path('routes/api.php'));
+
             Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+
+            Route::middleware('web')
+                ->prefix('leaves-application')
+                ->name('subfolder.')
                 ->group(base_path('routes/web.php'));
         });
     }
